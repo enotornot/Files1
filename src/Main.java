bcc82a52c6d59c462d97721edb0bae88d2ba3cb3
Marina Lezhnyova <t2d@ya.ru>
2022-05-04 21:36:08 +0300

    Рефакторинг

--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,62 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
         File workingDirGames = new File("D://Games/");
         StringBuilder log = new StringBuilder();
 
+        List<String> creatingDirs = new ArrayList<>(Arrays.asList(
+                "/savegames",
+                "/temp",
+                "/src/main",
+                "/src/test",
+                "/res/drawables",
+                "/res/vectors",
+                "/res/icons"
+        ));
 
-        String[] gameDir = {"/src", "/res", "/savegames", "/temp"};
-        String[] gameScrDir = {"/main", "/test"};
-        String[] gameResDir = {"/drawables", "/vectors", "/icons"};
+        List<String> creatingFiles = new ArrayList<>(Arrays.asList(
+                "/src/main/Main.java",
+                "/src/main/Utils.java",
+                "/temp/temp.txt"
+        ));
 
-        for (int i = 0; i < gameDir.length; i++) {
-            File directoryConstructor = new File((workingDirGames + gameDir[i]));
+        for (String newDir : creatingDirs) {
+            File directoryConstructor = new File((workingDirGames + newDir));
             if (directoryConstructor.mkdirs()) {
-                System.out.println("Создана директория " + gameDir[i] + ".");
-                log.append("Создана директория " + gameDir[i] + ". \n");
+                log.append("Создана директория " + newDir + ". \n");
             } else {
-                System.err.println("Не удалось создать директорию " + gameDir[i] + ".");
-                log.append("Не удалось создать директорию " + gameDir[i] + ". \n");
+                log.append("Не удалось создать директорию " + newDir + ". \n");
             }
         }
 
-        for (int i = 0; i < gameScrDir.length; i++) {
-            File directoryConstructor = new File((workingDirGames + "/src" + gameScrDir[i]));
-            if (directoryConstructor.mkdirs()) {
-                System.out.println("Создана директория " + gameScrDir[i] + ".");
-                log.append("Создана директория " + gameScrDir[i] + ". \n");
-            } else {
-                System.err.println("Не удалось создать директорию " + gameScrDir[i] + ".");
-                log.append("Не удалось создать директорию " + gameScrDir[i] + ". \n");
-            }
-        }
 
-        for (int i = 0; i < gameResDir.length; i++) {
-            File directoryConstructor = new File((workingDirGames + "/res" + gameResDir[i]));
-            if (directoryConstructor.mkdirs()) {
-                System.out.println("Создана директория " + gameResDir[i] + ".");
-                log.append("Создана директория " + gameResDir[i] + ". \n");
-            } else {
-                System.err.println("Не удалось создать директорию " + gameResDir[i] + ".");
-                log.append("Не удалось создать директорию " + gameResDir[i] + ". \n");
-            }
-        }
-
-        File workingDirGamesSrcMainToFile = new File("D://Games//src//main");
-
-        File mainJava = new File(workingDirGamesSrcMainToFile, "Main.java");
-
-        if (!mainJava.exists()) {
+        for (String newFile : creatingFiles) {
             try {
-                if (mainJava.createNewFile())
-                    System.out.println("Создан файл " + mainJava + ".");
-                log.append("Создан файл " + mainJava + ". \n");
+                File creatingFile = new File(workingDirGames + newFile);
+                if (!creatingFile.exists()) {
+                    if (creatingFile.createNewFile())
+                    log.append("Создан файл " + creatingFile + ". \n");
+                } else {
+                    log.append("Файл " + creatingFile + " уже существует \n");
+                }
             } catch (IOException ex) {
-                System.err.println("Не удалось создать файл " + mainJava + ".");
+                log.append("Файл не создан: " + ex.getMessage() + " \n");
             }
-        } else {
-            System.out.println("Файл " + mainJava + " уже существует.");
-            log.append("Файл " + mainJava + " уже существует \n");
-        }
 
-
-        File utilsJava = new File(workingDirGamesSrcMainToFile, "Utils.java");
-        if (!utilsJava.exists()) {
-            try {
-                if (utilsJava.createNewFile())
-                    System.out.println("Создан файл " + utilsJava + ".");
-                log.append("Создан файл " + utilsJava + ". \n");
-            } catch (IOException ex) {
-                System.err.println("Не удалось создать файл " + utilsJava + ".");
+            try (FileWriter logToFile = new FileWriter("D://Games//temp//temp.txt", true)) {
+                logToFile.write(String.valueOf(log));
+            } catch (IOException e) {
+                log.append(e.getMessage());
             }
-        } else {
-            System.out.println("Файл " + utilsJava + " уже существует.");
-            log.append("Файл " + utilsJava + " уже существует \n");
+
         }
-
-        File workingDirGamesTempToFile = new File("D://Games//temp");
-
-        File tempTxt = new File(workingDirGamesTempToFile, "temp.txt");
-
-        if (!tempTxt.exists()) {
-            try {
-                if (tempTxt.createNewFile())
-                    System.out.println("Создан файл " + tempTxt + ". \n");
-                log.append("Создан файл " + tempTxt + ".");
-            } catch (IOException ex) {
-                System.err.println("Не удалось создать файл " + tempTxt + ".");
-            }
-        } else {
-            System.out.println("Файл " + tempTxt + " уже существует.");
-            log.append("Файл " + tempTxt + " уже существует \n");
-        }
-
-
-        try (FileWriter logToFile = new FileWriter("D://Games//temp//temp.txt", true)) {
-            logToFile.write(String.valueOf(log));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 }
